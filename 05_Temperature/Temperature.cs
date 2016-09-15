c0a3d7924b4eca9e1ea6bfd9b10543cfd5af3702
Andre <andr9528@edu.eal.dk>
2016-09-15 13:24:16 +0200

    1st

---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Temperature
+{
+    class Temperature
+    {
+        internal static int FahrenheitToCelcius(double v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int CelciusToFahrenheit(double v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- 05_Temperature/TemperatureTest.cs
+++ 05_Temperature/TemperatureTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using _01_Temperature;
 
 namespace _05_Temperature
 {
